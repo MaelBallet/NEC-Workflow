8c12ac53d7c05a907fce89da9266b35c1274bcc9
Mael <56258943+MaelBallet@users.noreply.github.com>
2021-01-11 03:35:02 +0100

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,11 +2,11 @@
 
 # Train Stage 1 Models
 pushd phalanx
-./stage1.sh
+bash ./stage1.sh
 popd
 
 pushd bes
-./stage1.sh
+bash ./stage1.sh
 
 # Generate Pseudolabels
 python3 ensemble.py \
@@ -19,12 +19,12 @@ pushd phalanx
 
 # Generate pseudolabels for folds
 python3 make_pseudo.py
-./stage2.sh
+bash ./stage2.sh
 popd
 
 pushd bes
-./stage2_train.sh
-./stage2_predict.sh
+bash ./stage2_train.sh
+bash ./stage2_predict.sh
 
 # Generate Pseudolabels
 python3 ensemble.py \
@@ -34,5 +34,5 @@ popd
 
 # Train Stage 3 Models
 pushd phalanx
-./stage3_train.sh
+bash ./stage3_train.sh
 popd
